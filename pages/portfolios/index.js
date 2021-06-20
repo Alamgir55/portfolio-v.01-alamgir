07061c303f42defc27dcad6852cb7fdbfac81475
alamgir hossain <alamgir555hossain@gmail.com>
2021-06-19 23:21:19 -0400

    portfolio-profile

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,46 +1,39 @@
 
 import BaseLayout from '@/components/layouts/BaseLayout'
-//import Link from 'next/link'
-import Link from 'next/link'
+import { Row, Col} from 'reactstrap';
 import BasePage from '@/components/BasePage'
-import {useGetPosts} from '@/actions/index' 
 import {useGetUser} from '@/actions/user';
+import PortfolioApi from '@/lib/api/portfolios';
+import PortfolioCard from '@/components/PortfolioCard'
 
 
-const Portfolios = () => {
-    const { data, error, loading } = useGetPosts();
+const Portfolios = ({portfolios}) => {
     const {data: dataU, loading: loadingU } = useGetUser();
 
-
-    const renderPosts = (posts) =>{
-      return posts.map(post => 
-        <li key={post.id} style={{'fontSize': '20px'}}>
-          <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
-            <a>
-            {post.title}
-            </a>
-          </Link>
-        </li>
-      )
-    }
     return (
         <BaseLayout user={dataU} loading={loadingU}>
-          <BasePage>
-            <h1>I am Portfolios Page</h1>
-            { loading &&
-              <p>Loading....</p>
-            }
-            { data &&
-              <ul>
-              {renderPosts(data)}
-              </ul>
-            }
-            { error &&
-              <div className="alert alert-danger">{error.message}</div>
-            }
+          <BasePage className="portfolio-page">
+            <Row>
+              { portfolios.map(portfolio => 
+                <Col key={portfolio._id} md="4">
+                  <PortfolioCard portfolio={portfolio} />
+                </Col>
+                ) 
+              }
+            </Row>
           </BasePage>
         </BaseLayout>
     )
 } 
+// This function is called during the build time
+// Impoved performance of page
+// It will create static page with dynamic data
+export async function getStaticProps(){
+  const json = await new PortfolioApi().getAll();
+  const portfolios = json.data;
+  return {
+    props: {portfolios}
+  }
+}
 
 export default Portfolios
