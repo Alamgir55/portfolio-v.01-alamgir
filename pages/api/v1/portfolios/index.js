d1d8a5828a8020b560b05d54e6dc3d2e545a6d4a
alamgir hossain <alamgir555hossain@gmail.com>
2021-08-15 22:58:17 -0400

    error-data-error

--- pages/api/v1/portfolios/index.js
+++ pages/api/v1/portfolios/index.js
@@ -4,7 +4,8 @@ import auth0 from '@/utils/auth0'
 export default async function createPortfolio(req, res){
   try {
     const { accessToken } = await auth0.getSession(req)
-    const json = await new PortfolioApi(accessToken).createPortfolio(req.body);
+    console.log(accessToken)
+    const json = await new PortfolioApi(accessToken).create(req.body);
     return res.json(json.data);
   } catch(e) {
     console.log('We are getting error here')
